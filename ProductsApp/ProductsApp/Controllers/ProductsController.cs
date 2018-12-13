fb9c485014787b5e79dab56cf4eb4762beb3ee47
slachhar <shefali.lachhar@gmail.com>
2018-12-13 18:46:52 +0530

    first product app in web api 2.0

---
+++ ProductsApp/ProductsApp/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ProductsApp.Models;
+
+namespace ProductsApp.Controllers
+{
+	public class ProductsController : ApiController
+	{
+		Product[] products = new Product[]
+		{
+			new Product { Id = 1, name = "Tomato Soup", category = "Groceries", Price = 1 },
+			new Product { Id = 2, name = "Yo-yo", category = "Toys", Price = 3.75M },
+			new Product { Id = 3, name = "Hammer", category = "Hardware", Price = 16.99M }
+		};
+
+		public IEnumerable<Product> GetAllProducts()
+		{
+			return products;
+		}
+
+		public IHttpActionResult GetProduct(int id)
+		{
+			var product = products.FirstOrDefault((p) => p.Id == id);
+			if(product == null)
+			{
+				return NotFound();
+			}
+			return Ok(product);
+		}
+
+	}
+}
---
+++ ProductsApp/ProductsApp/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductsApp.Models
+{
+	public class Product
+	{
+		public int Id { get; set; }
+		public string name { get; set; }
+		public string category { get; set; }
+		public decimal Price { get; set; }
+	}
+}
